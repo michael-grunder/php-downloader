199e790495a84aaffd9ef6eb98441624c03a48fb
michael-grunder <michael.grunder@gmail.com>
2023-10-23 21:07:33 -0700

    Refactor logic, remove deps

---
+++ src/downloads.rs
@@ -0,0 +1,331 @@
+use anyhow::{anyhow, Result};
+use chrono::{DateTime, Utc};
+use futures::future::join_all;
+use indicatif::{ProgressBar, ProgressStyle};
+use reqwest::Client;
+use serde::{de, Deserialize, Deserializer};
+use std::{fmt, fs::File, io::Write};
+use tempfile::NamedTempFile;
+
+#[derive(Debug)]
+pub struct DownloadUrl {
+    pub version: Version,
+    pub url: String,
+    pub size: u64,
+    pub date: Option<DateTime<Utc>>,
+    pub age: String,
+}
+
+#[derive(Debug)]
+pub struct DownloadList {
+    client: Client,
+    major: u8,
+    minor: u8,
+    extension: Extension,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Extension {
+    GZ,
+    BZ,
+    XY,
+}
+
+#[derive(Debug, Copy, Clone, Eq)]
+pub struct Version {
+    pub major: u8,
+    pub minor: u8,
+    pub patch: u8,
+    pub had_patch: bool,
+}
+
+impl std::default::Default for Extension {
+    fn default() -> Self {
+        Self::BZ
+    }
+}
+
+impl std::str::FromStr for Extension {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match &*s.to_lowercase() {
+            "bz2" | "bz" => Ok(Self::BZ),
+            "gz" => Ok(Self::GZ),
+            "xy" => Ok(Self::XY),
+            _ => Err(anyhow!("Unknown extension")),
+        }
+    }
+}
+
+impl fmt::Display for Extension {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> fmt::Result {
+        let ext = match self {
+            Self::BZ => "bz2",
+            Self::GZ => "gz",
+            Self::XY => "xy",
+        };
+
+        write!(f, "{ext}")
+    }
+}
+
+impl std::str::FromStr for Version {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split('.').collect();
+
+        if parts.len() != 2 && parts.len() != 3 {
+            return Err(anyhow!("Invalid version string '{s}'"));
+        }
+
+        let major = parts[0]
+            .parse()
+            .map_err(|_| anyhow!("Invalid major version"))?;
+        let minor = parts[1]
+            .parse()
+            .map_err(|_| anyhow!("Invalid minor version"))?;
+        let patch = if parts.len() == 3 {
+            parts[2]
+                .parse()
+                .map_err(|_| anyhow!("Invalid patch version"))?
+        } else {
+            0
+        };
+
+        Ok(Self::new_ex(major, minor, patch, parts.len() == 3))
+    }
+}
+
+impl DownloadUrl {
+    pub fn new(version: Version, url: &str, size: u64, date: Option<DateTime<Utc>>) -> Self {
+        Self {
+            version,
+            url: url.to_string(),
+            size,
+            date,
+            age: date.map_or_else(String::new, |d| Self::get_age(&d)),
+        }
+    }
+
+    fn get_age(date: &DateTime<Utc>) -> String {
+        let now = Utc::now();
+        let duration = now.signed_duration_since(date);
+
+        let years = duration.num_days() / 365;
+        let remaining_days_year = duration.num_days() % 365;
+
+        let months = remaining_days_year / 30;
+        let remaining_days_month = remaining_days_year % 30;
+
+        let days = remaining_days_month;
+
+        let hours = duration.num_hours() % 24;
+        let minutes = duration.num_minutes() % 60;
+
+        let parts = if years > 0 {
+            vec![(years, "year"), (months, "month")]
+        } else if months > 0 {
+            vec![(months, "month"), (days, "day")]
+        } else if days > 0 {
+            vec![(days, "day"), (hours, "hour")]
+        } else {
+            vec![(hours, "hour"), (minutes, "minute")]
+        };
+
+        parts
+            .into_iter()
+            .filter(|(v, _)| v > &0)
+            .map(|(v, ident)| format!("{v} {ident}{}", if v > 1 { "s" } else { "" }))
+            .collect::<Vec<String>>()
+            .join(", ")
+    }
+}
+
+impl Version {
+    pub const fn new(major: u8, minor: u8, patch: u8) -> Self {
+        Self {
+            major,
+            minor,
+            patch,
+            had_patch: true,
+        }
+    }
+
+    pub const fn new_ex(major: u8, minor: u8, patch: u8, had_patch: bool) -> Self {
+        Self {
+            major,
+            minor,
+            patch,
+            had_patch,
+        }
+    }
+
+    fn get_temp_file() -> NamedTempFile {
+        NamedTempFile::new().expect("Can't create temporary file")
+    }
+
+    pub fn get_download_tuple(self, extension: Extension) -> (NamedTempFile, String) {
+        (Self::get_temp_file(), self.get_url(extension))
+    }
+
+    pub fn get_file_name(self, extension: Extension) -> String {
+        format!(
+            "php-{}.{}.{}.tar.{}",
+            self.major, self.minor, self.patch, extension
+        )
+    }
+
+    fn get_url(self, extension: Extension) -> String {
+        if self.major <= 7 && self.minor < 4 {
+            format!(
+                "https://museum.php.net/php{}/php-{}.{}.{}.tar.{}",
+                self.major, self.major, self.minor, self.patch, extension
+            )
+        } else {
+            format!(
+                "https://php.net/distributions/php-{}.{}.{}.tar.{}",
+                self.major, self.minor, self.patch, extension
+            )
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for Version {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        std::str::FromStr::from_str(&s).map_err(de::Error::custom)
+    }
+}
+
+impl std::fmt::Display for Version {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}.{}.{}", self.major, self.minor, self.patch)
+    }
+}
+
+impl PartialEq for Version {
+    fn eq(&self, other: &Self) -> bool {
+        self.major == other.major && self.minor == other.minor && self.patch == other.patch
+    }
+}
+
+impl PartialOrd for Version {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Version {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.major
+            .cmp(&other.major)
+            .then_with(|| self.minor.cmp(&other.minor))
+            .then_with(|| self.patch.cmp(&other.patch))
+    }
+}
+
+impl DownloadList {
+    pub fn new(major: u8, minor: u8, extension: Extension) -> Self {
+        Self {
+            client: Client::new(),
+            major,
+            minor,
+            extension,
+        }
+    }
+
+    async fn get_header(&self, version: Version) -> Result<Option<DownloadUrl>> {
+        let url = version.get_url(self.extension);
+        let res = self.client.head(&url).send().await?;
+
+        if res.status().is_success() {
+            let headers = res.headers();
+
+            let content_length = headers
+                .get(reqwest::header::CONTENT_LENGTH)
+                .and_then(|value| value.to_str().ok())
+                .and_then(|str_val| str_val.parse::<u64>().ok())
+                .unwrap_or(0);
+
+            let last_modified = headers
+                .get(reqwest::header::LAST_MODIFIED)
+                .and_then(|value| value.to_str().ok())
+                .and_then(|str_val| DateTime::parse_from_rfc2822(str_val).ok())
+                .map(|datetime| datetime.with_timezone(&Utc));
+
+            Ok(Some(DownloadUrl::new(
+                version,
+                &url,
+                content_length,
+                last_modified,
+            )))
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub async fn list(&self) -> Result<Vec<DownloadUrl>> {
+        let urls: Vec<_> = (0..30)
+            .map(|patch| Version::new(self.major, self.minor, patch))
+            .map(|version| self.get_header(version))
+            .collect();
+
+        let mut urls: Vec<_> = join_all(urls)
+            .await
+            .into_iter()
+            .filter_map(Result::ok)
+            .flatten()
+            .collect();
+
+        urls.sort_unstable_by(|b, a| b.version.cmp(&a.version));
+
+        Ok(urls)
+    }
+
+    pub async fn latest(&self) -> Result<Option<DownloadUrl>> {
+        let mut urls = self.list().await?;
+        Ok(urls.pop())
+    }
+
+    pub async fn get(&self, patch: u8) -> Result<Option<DownloadUrl>> {
+        let version = Version::new(self.major, self.minor, patch);
+        self.get_header(version).await
+    }
+
+    pub async fn download(&self, patch: u8, file: &mut std::fs::File) -> Result<()> {
+        let version = Version::new(self.major, self.minor, patch);
+        let url = version.get_url(self.extension);
+
+        let mut response = self.client.get(url).send().await?;
+
+        let total_size = response
+            .headers()
+            .get(reqwest::header::CONTENT_LENGTH)
+            .and_then(|val| val.to_str().ok())
+            .and_then(|val| val.parse::<u64>().ok())
+            .unwrap_or(0);
+
+        let tmpl = "{msg} {spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})";
+
+        let pb = ProgressBar::new(total_size);
+        pb.set_style(
+            ProgressStyle::default_bar()
+                .template(tmpl)?
+                .progress_chars("#>-"),
+        );
+        pb.set_message(version.to_string());
+
+        while let Some(chunk) = response.chunk().await? {
+            pb.inc(chunk.len() as u64);
+            file.write_all(&chunk)?;
+        }
+
+        pb.finish_with_message("download completed");
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ use anyhow::{Context, Result};
 use chrono::{DateTime, Utc};
 use clap::Parser;
 use colored::Colorize;
-use downloader::Downloader;
+use tempfile::NamedTempFile;
+//use downloader::Downloader;
 use indicatif::{ProgressBar, ProgressState, ProgressStyle};
 use num_format::{Locale, ToFormattedString};
 use std::{fmt, path::PathBuf};
@@ -35,103 +36,6 @@ struct Options {
     #[arg(default_value = ".")]
     output_path: Option<PathBuf>,
 }
-
-struct ProgressBarContainer {
-    progress_bar: ProgressBar,
-    data_len: u64,
-}
-
-// Define a custom progress reporter:
-struct SimpleReporterPrivate {
-    progress_bar: Option<ProgressBarContainer>,
-}
-
-struct SimpleReporter {
-    private: std::sync::Mutex<Option<SimpleReporterPrivate>>,
-}
-
-impl SimpleReporter {
-    #[cfg(not(feature = "tui"))]
-    fn create() -> std::sync::Arc<Self> {
-        std::sync::Arc::new(Self {
-            private: std::sync::Mutex::new(None),
-        })
-    }
-}
-
-const PB_TEMPLATE: &str =
-    "{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})";
-
-impl ProgressBarContainer {
-    fn progress_bar(max: u64) -> ProgressBar {
-        let progress_bar = ProgressBar::new(max);
-        progress_bar.set_style(
-            ProgressStyle::with_template(PB_TEMPLATE)
-                .unwrap()
-                .with_key("eta", |state: &ProgressState, w: &mut dyn fmt::Write| {
-                    write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap();
-                })
-                .progress_chars("#>-"),
-        );
-
-        progress_bar
-    }
-
-    pub fn new(data_len: u64) -> Self {
-        Self {
-            progress_bar: Self::progress_bar(data_len),
-            data_len,
-        }
-    }
-}
-
-impl downloader::progress::Reporter for SimpleReporter {
-    fn setup(&self, max_progress: Option<u64>, _: &str) {
-        let max_progress = max_progress.unwrap_or(0);
-
-        let progress_bar = if max_progress > 4096 {
-            Some(ProgressBarContainer::new(max_progress))
-        } else {
-            None
-        };
-
-        let private = SimpleReporterPrivate { progress_bar };
-
-        let mut guard = self.private.lock().unwrap();
-        *guard = Some(private);
-    }
-
-    fn progress(&self, current: u64) {
-        if let Some(p) = self.private.lock().unwrap().as_mut() {
-            if let Some(pb) = &p.progress_bar {
-                pb.progress_bar.set_position(current);
-            }
-        }
-    }
-
-    fn set_message(&self, _: &str) {}
-
-    fn done(&self) {
-        if let Some(p) = self.private.lock().unwrap().as_mut() {
-            if let Some(p) = &p.progress_bar {
-                p.progress_bar.set_position(p.data_len);
-            }
-        }
-
-        let mut guard = self.private.lock().unwrap();
-        *guard = None;
-    }
-}
-
-//fn print_download_url(du: &DownloadUrl) {
-//    println!(
-//        "{:>7} {} {} ({})",
-//        du.version.to_string().bold(),
-//        "->".green(),
-//        du.url,
-//        du.age
-//    );
-//}
 
 fn print_download_urls(urls: &[DownloadUrl]) {
     // Calculating the maximum lengths of each field in a more idiomatic way
@@ -185,44 +89,25 @@ async fn main() -> Result<()> {
         print_download_urls(&urls);
         std::process::exit(0);
     } else {
-        let mut downloader = Downloader::builder().build()?;
-        let (file, url) = opt.version.get_download_tuple(opt.extension);
+        let downloads = DownloadList::new(opt.version.major, opt.version.minor, opt.extension);
 
-        let mut dl = downloader::Download::new(&url);
-        dl = dl.file_name(file.path());
-        dl = dl.progress(SimpleReporter::create());
-        let downloads: Vec<downloader::Download> = vec![dl];
+        let version = Version::new(
+            opt.version.major,
+            opt.version.minor,
+            if opt.version.had_patch {
+                opt.version.patch
+            } else {
+                downloads.latest().await?.unwrap().version.patch
+            },
+        );
 
-        let result = downloader.download(&downloads)?;
+        let mut tmp = NamedTempFile::new()?;
+        downloads.download(version.patch, tmp.as_file_mut()).await?;
 
-        if let Some(summary) = result.into_iter().flatten().next() {
-            let mut path = opt.output_path.clone().unwrap();
-            path.push(opt.version.get_file_name(opt.extension));
-            println!("{:?} -> {path:?}", summary.file_name);
-            std::fs::rename(summary.file_name, path)?;
-            std::process::exit(0);
-        }
+        let mut dst = opt.output_path.unwrap();
+        dst.push(version.get_file_name(opt.extension));
+        tmp.persist(&dst)?;
     }
 
-    //let mut downloader = Downloader::builder().parallel_requests(2).build().unwrap();
-    //let mut downloads: Vec<downloader::Download> = vec![];
-
-    //for (file, url) in opt.version.get_urls() {
-    //    let mut dl = downloader::Download::new(&url);
-    //    dl = dl.file_name(file.path());
-    //    dl = dl.progress(SimpleReporter::create());
-    //    downloads.push(dl);
-    //}
-
-    //let result = downloader.download(&downloads)?;
-
-    //for summary in result.into_iter().flatten() {
-    //    let mut path = opt.output_path.unwrap();
-    //    path.push(opt.version.get_file_name());
-    //    println!("{:?} -> {path:?}", summary.file_name);
-    //    std::fs::rename(summary.file_name, path)?;
-    //    std::process::exit(0);
-    //}
-
-    std::process::exit(1);
+    std::process::exit(0);
 }
