cd9a23eb39cc7eec9b283ccf41070fdcd3fc0e74
michael-grunder <michael.grunder@gmail.com>
2023-10-31 17:15:54 -0700

    Commit additional modules.

---
+++ src/extract.rs
@@ -0,0 +1,176 @@
+use crate::{
+    downloads::{DownloadInfo, Extension, Version},
+    view::ToHumanSize,
+};
+use anyhow::{anyhow, Result};
+use bzip2::read::BzDecoder;
+use flate2::read::GzDecoder;
+use indicatif::ProgressBar;
+use std::{
+    convert::From,
+    fs::File,
+    io::{self, Read},
+    path::{Path, PathBuf},
+    result::Result as StdResult,
+};
+use tar::Archive;
+use xz::read::XzDecoder;
+
+#[derive(Debug)]
+pub struct Tarball {
+    src: PathBuf,
+    ext: Extension,
+}
+
+struct ProgressReader<R> {
+    reader: R,
+    progress_bar: ProgressBar,
+}
+
+pub trait Extract {
+    fn extract(&self, dst_root: &Path, dst_leaf: Option<&Path>) -> Result<()>;
+}
+
+impl Tarball {
+    // fn progress_bar(total_size: u64) -> Result<ProgressBar> {
+    //     let tmpl = "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})";
+
+    //     let pb = ProgressBar::new(total_size);
+
+    //     pb.set_style(
+    //         ProgressStyle::default_bar()
+    //             .template(tmpl)?
+    //             .progress_chars("#>-"),
+    //     );
+
+    //     Ok(pb)
+    // }
+
+    fn progress_spinner(&self, size: u64, dst: &Path) -> Result<ProgressBar> {
+        let file = self
+            .src
+            .file_name()
+            .ok_or_else(|| anyhow!("Can't get filename"))?
+            .to_string_lossy();
+
+        let pb = ProgressBar::new_spinner();
+
+        pb.set_message(format!(
+            "{file} ({}) -> {}",
+            size.to_human_size(),
+            dst.display(),
+        ));
+
+        Ok(pb)
+    }
+
+    fn clean_file_name(&self) -> Result<PathBuf> {
+        let file = self
+            .src
+            .file_name()
+            .ok_or_else(|| anyhow!("Can't get filename"))?
+            .to_string_lossy()
+            .into_owned();
+
+        if let Some((version, _)) = file.split_once(".tar") {
+            Ok(PathBuf::from(version))
+        } else {
+            anyhow::bail!("Unable to determine stem for '{}'", file);
+        }
+    }
+
+    fn full_path(root: &Path, leaf: &Path) -> PathBuf {
+        let mut full: PathBuf = root.to_path_buf();
+        full.push(leaf);
+        full
+    }
+
+    pub fn list(dir: &Path) -> Result<Vec<DownloadInfo>> {
+        let mut res = vec![];
+
+        for entry in std::fs::read_dir(dir)?.filter_map(StdResult::ok) {
+            let path = entry.path();
+
+            if !path.is_dir() {
+                DownloadInfo::from_file(&path).map_or_else(
+                    |_| {
+                        eprintln!("Can't parse file '{path:?}'");
+                    },
+                    |info| {
+                        res.push(info);
+                    },
+                );
+            }
+        }
+
+        Ok(res)
+    }
+
+    pub fn matching(path: &Path, version: Version) -> Result<Vec<DownloadInfo>> {
+        Ok(Self::list(path)?
+            .into_iter()
+            .filter(|fi| fi.version.matches(version))
+            .collect())
+    }
+
+    pub fn latest(path: &Path, version: Version) -> Result<Option<DownloadInfo>> {
+        Ok(Self::matching(path, version)?.pop())
+    }
+}
+
+impl From<&DownloadInfo> for Tarball {
+    fn from(src: &DownloadInfo) -> Self {
+        Self {
+            src: PathBuf::from(&src.location),
+            ext: src.extension,
+        }
+    }
+}
+
+impl From<DownloadInfo> for Tarball {
+    fn from(src: DownloadInfo) -> Self {
+        Self {
+            src: PathBuf::from(src.location),
+            ext: src.extension,
+        }
+    }
+}
+
+impl Extract for Tarball {
+    fn extract(&self, dst_root: &Path, dst_leaf: Option<&Path>) -> Result<()> {
+        let file = File::open(&self.src)?;
+        let total_size = file.metadata()?.len();
+
+        let decoder: Box<dyn Read> = match self.ext {
+            Extension::GZ => Box::new(GzDecoder::new(file)),
+            Extension::BZ => Box::new(BzDecoder::new(file)),
+            Extension::XZ => Box::new(XzDecoder::new(file)),
+        };
+
+        let tmp = tempfile::tempdir()?;
+        let def = self.clean_file_name()?;
+        let src = Self::full_path(tmp.path(), &def);
+        let dst = Self::full_path(dst_root, dst_leaf.unwrap_or(&def));
+
+        let reader = ProgressReader {
+            reader: decoder,
+            progress_bar: self.progress_spinner(total_size, &dst)?,
+        };
+
+        let mut archive = Archive::new(reader);
+        archive.unpack(&tmp)?;
+
+        std::fs::rename(src, &dst)?;
+        eprintln!("Files extracted to '{}'", dst.display());
+
+        Ok(())
+    }
+}
+
+impl<R: Read> Read for ProgressReader<R> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        let bytes = self.reader.read(buf)?;
+        self.progress_bar.tick();
+        Ok(bytes)
+    }
+}
---
+++ src/view.rs
@@ -0,0 +1,106 @@
+use crate::{downloads::DownloadInfo, Version};
+use chrono::{DateTime, Utc};
+use colored::Colorize;
+use serde_json::to_string_pretty;
+
+trait Displayable {}
+
+pub trait Viewer: Send + Sync {
+    fn display(&self, data: &[DownloadInfo]);
+}
+
+pub trait Download {
+    fn version(&self) -> Version;
+    fn path(&self) -> String;
+    fn size(&self) -> usize;
+    fn date(&self) -> Option<DateTime<Utc>>;
+
+    fn date_string(&self) -> String {
+        self.date()
+            .map_or_else(String::new, |d| d.format("%d %b %y").to_string())
+    }
+}
+
+struct CliViewer;
+struct JsonViewer;
+
+impl<T: Into<u64>> ToHumanSize for T {
+    fn to_human_size(self) -> String {
+        Self::to_human_size_fmt(self.into())
+    }
+}
+
+pub trait ToHumanSize {
+    fn to_human_size(self) -> String;
+
+    fn to_human_size_fmt(v: u64) -> String {
+        let (val, unit) = Self::to_human_size_impl(v);
+        format!("{val:.2} {unit}")
+    }
+
+    fn to_human_size_impl(v: u64) -> (f64, &'static str) {
+        const KB: f64 = 1024.0;
+        const MB: f64 = KB * 1024.0;
+        const GB: f64 = MB * 1024.0;
+        const TB: f64 = GB * 1024.0;
+
+        #[allow(clippy::cast_precision_loss)]
+        let v = v as f64;
+
+        if v < KB {
+            (v, "B")
+        } else if v < MB {
+            (v / KB, "KB")
+        } else if v < GB {
+            (v / MB, "MB")
+        } else if v < TB {
+            (v / GB, "GB")
+        } else {
+            (v / TB, "TB")
+        }
+    }
+}
+
+impl Viewer for CliViewer {
+    fn display(&self, urls: &[DownloadInfo]) {
+        // Calculating the maximum lengths of each field in a more idiomatic way
+        let max_lens = urls.iter().fold([0, 0, 0, 0], |mut acc, url| {
+            acc[0] = acc[0].max(url.version.to_string().len());
+            acc[1] = acc[1].max(url.size.to_human_size().len());
+            acc[2] = acc[2].max(url.date_string().len());
+            acc[3] = acc[3].max(url.location.len());
+            acc
+        });
+
+        // Printing each url with fields aligned based on their maximum lengths
+        // "{:<width0$} \u{2502} {:<width1$} {:>width2$} \u{2192} {:<width3$}",
+        for url in urls {
+            println!(
+                "{:<width0$} - {:<width1$} {:>width2$} {:<width3$}",
+                url.version.to_string().bold(),
+                url.size.to_human_size(),
+                url.date_string(),
+                url.location,
+                width0 = max_lens[0],
+                width1 = max_lens[1],
+                width2 = max_lens[2],
+                width3 = max_lens[3],
+            );
+        }
+    }
+}
+
+impl Viewer for JsonViewer {
+    fn display(&self, urls: &[DownloadInfo]) {
+        let s = to_string_pretty(urls).unwrap_or_else(|_| String::from("Error generating JSON"));
+        println!("{s}");
+    }
+}
+
+pub fn get_viewer(json: bool) -> Box<dyn Viewer + Send + Sync> {
+    if json {
+        Box::new(JsonViewer)
+    } else {
+        Box::new(CliViewer)
+    }
+}
